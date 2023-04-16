d8417b5581c9ece1caca1ad6ea156e0e61900187
bb0lshak0vv <58132397+bb0lshak0vv@users.noreply.github.com>
2023-04-16 22:08:10 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -11,7 +11,7 @@ class TPQueue {
   SYM m[5];
   int count;
   int head;
-  
+ 
  public:
   TPQueue() {
     count = 0;
@@ -19,13 +19,12 @@ class TPQueue {
     for (int i = 0; i < size; i++) {
       m[i].prior = 0;
     }
-	}
+  }
   void push(T elm) {
     int pos = 0;
     if (count == size) {
-      cout << Count() << endl;
-			return;
-		}
+      return;
+    }
     if (count == 0 && head == 0) {
       pos = 0;
       m[0] = elm;
@@ -40,45 +39,45 @@ class TPQueue {
           m[pos] = elm;
           count++;
           break;
-				}
-			}
-		}
-		return;
-	}
+        }
+      }
+    }
+    return;
+  }
   T pop() {
     if (count == 0) {
-      cout << Count() << endl;
-      return SYM{'0',0};
-		}
+      return SYM{'0', 0};
+    }
     SYM Buf = m[head];
     m[head].ch = 0;
     m[head].prior = 0;
     head++;
     if (head > size-1)
       head = 0;
-		count--;
+    count--;
     if (count == 0)
       head = 0;
     return Buf;
-	}
+  }
   void UpHeaval(int pos) {
-		int end = head + count;
-		if (end > size - 1)
+    int end = head + count;
+    if (end > size - 1)
       end = end - size;
     int num = head + count - pos;
     if (num >= size)
       num = num - size;
     for (int i = 0; i < num; i++) {
-			if (end == 0) {
+      if (end == 0) {
         m[0] = m[size - 1];
         end = size-1;
       } else {
         m[end] = m[end - 1];
         end--;
-			}
-		}
-		return;
-	}
+      }
+    }
+    return;
+  }
 };
+
 
 #endif  // INCLUDE_TPQUEUE_H_
