03c3414b06fa69186ffdea18c59e197bfa1146aa
bb0lshak0vv <58132397+bb0lshak0vv@users.noreply.github.com>
2023-04-16 22:21:04 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,83 +1,52 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
+
+template<typename T, int size>
+class TPQueue {
+ private:
+   T* arr;
+   int last, first, count;
+   
+ public:
+   TPQueue() :first(0), last(0), count(0) { arr = new T[size]; }
+   bool isEmpty() const {
+       return 0 == count;
+   }
+   bool isFull() const {
+      return count == size;
+   }
+   const T& pop() {
+       if (isEmpty()) {
+           throw std::string("Empty");
+       } else {
+           count--;
+           return arr[first++ % size];
+       }
+   }
+   void push(const T& value) {
+       if (isFull()) {
+           throw std::string("Full");
+       } else {
+           int x = last;
+           arr[last % size] = value;
+           T temp = arr[x % size];
+           while (arr[x % size].prior > \
+                arr[(x - 1) % size].prior && x > first) {
+                temp = arr[x % size];
+                arr[x % size] = arr[(x - 1) % size];
+                arr[(x - 1) % size] = temp;
+                x--;
+            }
+            count++;
+            last++;
+        }
+    }
+};
 struct SYM {
   char ch;
   int prior;
 };
-template<typename T, int size>
-class TPQueue {
- private:
-  SYM m[5];
-  int count;
-  int head;
- 
- public:
-  TPQueue() {
-    count = 0;
-    head = 0;
-    for (int i = 0; i < size; i++) {
-      m[i].prior = 0;
-    }
-  }
-  void push(T elm) {
-    int pos = 0;
-    if (count == size) {
-      return;
-    }
-    if (count == 0 && head == 0) {
-      pos = 0;
-      m[0] = elm;
-      count++;
-    } else {
-      for (int i = 0; i <= count; i++) {
-        pos = head + i;
-        if (pos > size - 1)
-          pos = pos - size;
-        if (m[pos].prior < elm.prior) {
-          UpHeaval(pos);
-          m[pos] = elm;
-          count++;
-          break;
-        }
-      }
-    }
-    return;
-  }
-  T pop() {
-    if (count == 0) {
-      return SYM{'0', 0};
-    }
-    SYM Buf = m[head];
-    m[head].ch = 0;
-    m[head].prior = 0;
-    head++;
-    if (head > size-1)
-      head = 0;
-    count--;
-    if (count == 0)
-      head = 0;
-    return Buf;
-  }
-  void UpHeaval(int pos) {
-    int end = head + count;
-    if (end > size - 1)
-      end = end - size;
-    int num = head + count - pos;
-    if (num >= size)
-      num = num - size;
-    for (int i = 0; i < num; i++) {
-      if (end == 0) {
-        m[0] = m[size - 1];
-        end = size-1;
-      } else {
-        m[end] = m[end - 1];
-        end--;
-      }
-    }
-    return;
-  }
-};
 
-
-#endif  // INCLUDE_TPQUEUE_H_
+#endif // INCLUDE_TPQUEUE_H_
